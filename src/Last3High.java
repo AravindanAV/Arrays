ac2eaad9bf16abd8bbe8b6b746310ee853ea7feb
AravindanAV <aravindrajyasri@gmail.com>
2023-06-03 00:56:33 +0530

    find three largest gpt comments

--- src/Last3High.java
+++ src/Last3High.java
@@ -1,6 +1,6 @@
 public class Last3High {
     public static void main(String[] args) {
-        int []arr = {2,44,22,55,66,1,5,8,88,99,64,3,4,100};
+        int []arr = {2,44,22,55,66,1,5,8,88,92,64,3,4,110,105,98,102,88,89};
         findThreeLargest(arr);
     }
 
@@ -13,6 +13,11 @@ public class Last3High {
                 high3=high2;
                 high2= high1;
                 high1 = val;
+            } else if (val > high2) {
+                high3=high2;
+                high2=val;
+            } else if (val > high3) {
+                high3 = val;
             }
 
         }
