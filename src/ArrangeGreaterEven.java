ed9bb6c0dd067b42b8fc6d622f86237767b60662
AravindanAV <aravindrajyasri@gmail.com>
2023-06-03 13:44:11 +0530

    shifter greater even

---
+++ src/ArrangeGreaterEven.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ArrangeGreaterEven {
+
+    public static void main(String[] args) {
+        int []arr = {4,1,5,2,1,6,9,8,0,7};
+        rearrangeGreaterEven(arr);
+    }
+
+    static void rearrangeGreaterEven(int []arr){
+        int j = 0;
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            j=i+1;
+            if (arr[i]>arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            i = j++;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
